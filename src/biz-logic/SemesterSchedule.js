bfe366c1b1a607cdaa918e2d771f29a7c085559f
Jordan Pratt <jpratt@mtroyal.ca>
2018-06-09 18:39:07 +0000

    Add Winter 2019 as default startup.

--- src/biz-logic/SemesterSchedule.js
+++ src/biz-logic/SemesterSchedule.js
@@ -1,8 +1,8 @@
-// const ScheduledClassesApi = require("../api/ScheduledClassesApi");
+const ScheduledClassesApi = require("../api/ScheduledClassesApi");
 
 const SemesterSchedule = (module.exports = function() {
-  // const api = new ScheduledClassesApi();
-  this.classroomEvents = []; // api.classes();
+  const api = new ScheduledClassesApi();
+  this.classroomEvents = api.classes();
 });
 
 SemesterSchedule.prototype.events = function() {
